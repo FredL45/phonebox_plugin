30a7300af68205d0132f1ef6e6c115e6a32dc36f
FredL45 <120400402+FredL45@users.noreply.github.com>
2022-12-12 17:01:59 +0100

    Update nested_serializers.py
    correction for netbox 3.3 support

--- phonebox_plugin/api/nested_serializers.py
+++ phonebox_plugin/api/nested_serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from phonebox_plugin import models
+# for netbox 3.3
+try:
 from netbox.api import WritableNestedSerializer
+except ImportError:
+from netbox.api.serializers.nested import WritableNestedSerializer
+
 from tenancy.api.nested_serializers import NestedTenantSerializer
 
 __all__ = ["NestedNumberSerializer", ]
